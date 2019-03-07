81b6280c9e10c02f0d1d7bec8ac15d214bd48be1
Frank Gregor <phranck@cocoanaut.com>
2019-03-07 20:52:42 +0100

    rewrites entire main menu creation

--- App/Podlive/Categories/Appkit/NSApplication+MainMenu.m
+++ App/Podlive/Categories/Appkit/NSApplication+MainMenu.m
@@ -6,6 +6,7 @@
 @import CCNAppKit;
 #import "NSApplication+MainMenu.h"
 #import "CCNApplicationDelegate.h"
+#import "NSBundle+Localization.h"
 
 
 #pragma clang diagnostic push
@@ -15,165 +16,16 @@
 
 - (void)populateMainMenu {
     let mainMenu = [[NSMenu alloc] initWithTitle:@"MainMenu"];
+    mainMenu.autoenablesItems = YES;
 
-    // The titles of the menu items are for identification purposes only and shouldn't be localized.
-    // The strings in the menu bar come from the submenu titles,
-    // except for the application menu, whose title is ignored at runtime.
-
-    var menuItem = [mainMenu addItemWithTitle:@"Application" action:nil keyEquivalent:@""];
-    var submenu = [[NSMenu alloc] initWithTitle:@"Application"];
-    [self _populateApplicationMenu:submenu];
-    [mainMenu setSubmenu:submenu forItem:menuItem];
-
-    menuItem = [mainMenu addItemWithTitle:@"File" action:nil keyEquivalent:@""];
-    submenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Podcasts", @"The File menu")];
-    [self _populateFileMenu:submenu];
-    [mainMenu setSubmenu:submenu forItem:menuItem];
-
-    menuItem = [mainMenu addItemWithTitle:@"Edit" action:nil keyEquivalent:@""];
-    submenu = [[NSMenu alloc] initWithTitle:NSLocalizedString( @"Edit", @"The Edit menu" )];
-    [self _populateEditMenu:submenu];
-    [mainMenu setSubmenu:submenu forItem:menuItem];
-
-    menuItem = [mainMenu addItemWithTitle:@"View" action:nil keyEquivalent:@""];
-    submenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"View", @"The View menu")];
-    [self _populateViewMenu:submenu];
-    [mainMenu setSubmenu:submenu forItem:menuItem];
-
-    menuItem = [mainMenu addItemWithTitle:@"Window" action:nil keyEquivalent:@""];
-    submenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Window", @"The Window menu")];
-    [self _populateWindowMenu:submenu];
-    [mainMenu setSubmenu:submenu forItem:menuItem];
-    NSApp.windowsMenu = submenu;
-
-//    let helpMenu = [[NSMenu alloc] initWithTitle:@"HelpMenu"];
-//    menuItem = [helpMenu addItemWithTitle:@"Help" action:nil keyEquivalent:@""];
-//    submenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Help", @"The Help menu")];
-//    [self _populateHelpMenu:submenu];
-//    [helpMenu setSubmenu:submenu forItem:menuItem];
-//    NSApp.helpMenu = submenu;
+    [mainMenu addItem:[self _applicationSubmenu]];
+    [mainMenu addItem:[self _podcastsSubmenu]];
+    [mainMenu addItem:[self _editSubmenu]];
+    [mainMenu addItem:[self _viewSubmenu]];
+    [mainMenu addItem:[self _windowSubmenu]];
+    [mainMenu addItem:[self _helpSubmenu]];
 
     NSApp.mainMenu = mainMenu;
-}
-
-- (void)_populateApplicationMenu:(NSMenu *)menu {
-    let applicationName = NSApplication.applicationName;
-    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
-
-    var menuTitle = [NSString stringWithFormat:NSLocalizedString(@"About %@", @"Main Menu Item Title"), applicationName];
-    var menuItem = [menu addItemWithTitle:menuTitle action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
-    [menuItem setTarget:NSApp];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Preferences", @"Main Menu Item Title") action:@selector(populatePreferencesWindow:) keyEquivalent:@","];
-    menuItem.target = appDelegate;
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Services", @"Main Menu Item Title") action:nil keyEquivalent:@""];
-
-    let servicesMenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Services", @"Main Menu Item Title")];
-    [menu setSubmenu:servicesMenu forItem:menuItem];
-    [NSApp setServicesMenu:servicesMenu];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    menuTitle = [NSString stringWithFormat:NSLocalizedString(@"%@ Hide", @"Main Menu Item Title"), applicationName];
-    menuItem = [menu addItemWithTitle:menuTitle action:@selector(hide:) keyEquivalent:@"h"];
-    [menuItem setTarget:NSApp];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Hide Others", @"Main Menu Item Title") action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
-    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagOption];
-    [menuItem setTarget:NSApp];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Show All", @"Main Menu Item Title") action:@selector(unhideAllApplications:) keyEquivalent:@""];
-    [menuItem setTarget:NSApp];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    menuTitle = [NSString stringWithFormat:NSLocalizedString(@"Quit %@", @"Main Menu Item Title"), applicationName];
-    menuItem = [menu addItemWithTitle:menuTitle action:@selector(terminate:) keyEquivalent:@"q"];
-    [menuItem setTarget:NSApp];
-}
-
-- (void)_populateFileMenu:(NSMenu *)menu {
-    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
-
-    var menuTitle = NSLocalizedString(@"Available Podcasts", @"Main Menu Item Title");
-    var menuItem = [menu addItemWithTitle:menuTitle action:@selector(showAvailablePodcasts) keyEquivalent:@"1"];
-    [menuItem setTarget:appDelegate.appViewController];
-
-    [menu addItem:NSApp.subscribedPodcastsMenuItem];
-    [menuItem setTarget:appDelegate.appViewController];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    [menu addItemWithTitle:NSLocalizedString(@"Close Window", @"Main Menu Item Title") action:@selector(performClose:) keyEquivalent:@"w"];
-}
-
-- (void)_populateEditMenu:(NSMenu *)menu {
-    [menu addItemWithTitle:NSLocalizedString(@"Cut", @"Main Menu Item Title") action:@selector(cut:) keyEquivalent:@"x"];
-    [menu addItemWithTitle:NSLocalizedString(@"Copy", @"Main Menu Item Title") action:@selector(copy:) keyEquivalent:@"c"];
-    [menu addItemWithTitle:NSLocalizedString(@"Paste", @"Main Menu Item Title") action:@selector(paste:) keyEquivalent:@"v"];
-
-    var menuItem = [menu addItemWithTitle:NSLocalizedString( @"Paste and Match Style", nil ) action:@selector( pasteAsPlainText: ) keyEquivalent:@"V"];
-    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagOption];
-
-    [menu addItemWithTitle:NSLocalizedString(@"Delete", @"Main Menu Item Title") action:@selector(delete:) keyEquivalent:[NSString stringWithFormat:@"%C", (unichar)NSBackspaceCharacter]];
-    [menu addItemWithTitle:NSLocalizedString(@"Select All", @"Main Menu Item Title") action:@selector(selectAll:) keyEquivalent:@"a"];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Find", @"Main Menu Item Title") action:nil keyEquivalent:@""];
-
-    let findMenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Find", @"Main Menu Item Title")];
-    [self _populateFindMenu:findMenu];
-    [menu setSubmenu:findMenu forItem:menuItem];
-}
-
-- (void)_populateViewMenu:(NSMenu *)menu {
-    var menuItem = [menu addItemWithTitle:NSLocalizedString(@"Enter Full Screen", @"Main Menu Item Title") action:@selector(toggleFullScreen:) keyEquivalent:@"f"];
-    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagControl];
-}
-
-- (void)_populateWindowMenu:(NSMenu *)menu {
-    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
-
-    [menu addItemWithTitle:NSLocalizedString(@"Minimize", @"Main Menu Item Title") action:@selector(performMiniaturize:) keyEquivalent:@"m"];
-    [menu addItemWithTitle:NSLocalizedString(@"Zoom", @"Main Menu Item Title") action:@selector(performZoom:) keyEquivalent:@""];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    var menuItem = [menu addItemWithTitle:NSLocalizedString(@"Podcast Overview", @"Main Menu Item Title") action:@selector(populateMainWindow) keyEquivalent:@"0"];
-    [menuItem setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagShift];
-    menuItem.target = appDelegate;
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    [menu addItemWithTitle:NSLocalizedString(@"Bring All to Front", @"Main Menu Item Title") action:@selector(arrangeInFront:) keyEquivalent:@""];
-}
-
-- (void)_populateHelpMenu:(NSMenu *)menu {
-    let menuTitle = [NSString stringWithFormat:@"%@ %@", NSApplication.applicationName, NSLocalizedString( @"Help", @"Help Menu Item Title" )];
-    var menuItem = [menu addItemWithTitle:menuTitle action:@selector(showHelp:) keyEquivalent:@"?"];
-    menuItem.target = NSApp;
-}
-
-- (void)_populateFindMenu:(NSMenu *)menu {
-    var menuItem = [menu addItemWithTitle:NSLocalizedString(@"Find…", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"f"];
-    [menuItem setTag:NSFindPanelActionShowFindPanel];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Find Next", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"g"];
-    [menuItem setTag:NSFindPanelActionNext];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Find Previous", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"G"];
-    [menuItem setTag:NSFindPanelActionPrevious];
-
-    menuItem = [menu addItemWithTitle:NSLocalizedString(@"Use Selection for Find", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"e"];
-    [menuItem setTag:NSFindPanelActionSetFindString];
-
-    [menu addItemWithTitle:NSLocalizedString(@"Jump to Selection", @"Main Menu Item Title") action:@selector(centerSelectionInVisibleArea:) keyEquivalent:@"j"];
 }
 
 // MARK: - Custom Accessors
@@ -187,6 +39,182 @@
                                         keyEquivalent:@"2"];
     });
     return _menuItem;
+}
+
+// MARK: - Build Submenus
+
+- (NSMenuItem *)_applicationSubmenu {
+    let applicationName = NSApplication.applicationName;
+    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
+
+    let aboutMenuItem = [self _menuItemWithTitle:[NSString stringWithFormat:NSLocalizedString(@"About %@", @"Main Menu Item Title"), applicationName] action:@selector(orderFrontStandardAboutPanel:) target:NSApp];
+    let prefsMenuItem = [self _menuItemWithTitle:NSLocalizedString(@"Preferences…", @"Main Menu Item Title") action:@selector(populatePreferencesWindow:) target:appDelegate];
+    prefsMenuItem.keyEquivalent = @",";
+    prefsMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand;
+
+    let servicesSubMenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Services", @"Main Menu Item Title")];
+    NSApp.servicesMenu = servicesSubMenu;
+    let servicesMenuItem = NSMenuItem.new;
+    servicesMenuItem.title = servicesSubMenu.title;
+    servicesMenuItem.submenu = servicesSubMenu;
+
+    let hideMenuItem = [self _menuItemWithTitle:[NSString stringWithFormat:NSLocalizedString(@"%@ Hide", @"Main Menu Item Title"), applicationName] action:@selector(hide:) target:NSApp];
+    hideMenuItem.keyEquivalent = @"h";
+
+    let hideOthersMenuItem = [self _menuItemWithTitle:NSLocalizedString(@"Hide Others", @"Main Menu Item Title") action:@selector(hideOtherApplications:) target:NSApp];
+    hideOthersMenuItem.keyEquivalent = @"h";
+    hideOthersMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand | NSEventModifierFlagOption;
+
+    let showAllMenuItem = [self _menuItemWithTitle:NSLocalizedString(@"Show All", @"Main Menu Item Title") action:@selector(unhideAllApplications:) target:NSApp];
+    let quitMenuItem = [self _menuItemWithTitle:[NSString stringWithFormat:NSLocalizedString(@"Quit %@", @"Main Menu Item Title"), applicationName] action:@selector(terminate:) target:NSApp];
+    quitMenuItem.keyEquivalent = @"q";
+
+    // Build submenu
+    let menu = NSMenu.new;
+    [menu addItem:aboutMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+    [menu addItem:prefsMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+    [menu addItem:servicesMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+    [menu addItem:hideMenuItem];
+    [menu addItem:hideOthersMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+    [menu addItem:showAllMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+    [menu addItem:quitMenuItem];
+
+    let menuItem = NSMenuItem.new;
+    menuItem.submenu = menu;
+
+    return menuItem;
+}
+
+- (NSMenuItem *)_podcastsSubmenu {
+    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
+
+    let availablePodcastsMenuItem = [self _menuItemWithTitle:NSLocalizedString(@"Available Podcasts", @"Main Menu Item Title") action:@selector(showAvailablePodcasts) target:appDelegate.appViewController];
+    availablePodcastsMenuItem.keyEquivalent = @"1";
+    availablePodcastsMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand;
+
+    let subscribedPodcastsMenuItem = NSApp.subscribedPodcastsMenuItem;
+
+    // Build submenu
+    let menu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Podcasts", @"The File menu")];
+    [menu addItem:availablePodcastsMenuItem];
+    [menu addItem:subscribedPodcastsMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+
+    let closeWindowMenuItem = [menu addItemWithTitle:NSLocalizedString(@"Close Window", @"Main Menu Item Title") action:@selector(performClose:) keyEquivalent:@"w"];
+    closeWindowMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand;
+
+    let menuItem = NSMenuItem.new;
+    menuItem.submenu = menu;
+
+    return menuItem;
+}
+
+- (NSMenuItem *)_editSubmenu {
+    // Build submenu
+    let menu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Edit", @"The File menu")];
+    [menu addItemWithTitle:NSLocalizedString(@"Cut", @"Main Menu Item Title") action:@selector(cut:) keyEquivalent:@"x"];
+    [menu addItemWithTitle:NSLocalizedString(@"Copy", @"Main Menu Item Title") action:@selector(copy:) keyEquivalent:@"c"];
+    [menu addItemWithTitle:NSLocalizedString(@"Paste", @"Main Menu Item Title") action:@selector(paste:) keyEquivalent:@"v"];
+
+    var subMenuItem = [menu addItemWithTitle:NSLocalizedString( @"Paste and Match Style", nil ) action:@selector( pasteAsPlainText: ) keyEquivalent:@"V"];
+    subMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand | NSEventModifierFlagOption;
+
+    [menu addItemWithTitle:NSLocalizedString(@"Delete", @"Main Menu Item Title") action:@selector(delete:) keyEquivalent:[NSString stringWithFormat:@"%C", (unichar)NSBackspaceCharacter]];
+    [menu addItemWithTitle:NSLocalizedString(@"Select All", @"Main Menu Item Title") action:@selector(selectAll:) keyEquivalent:@"a"];
+
+    [menu addItem:NSMenuItem.separatorItem];
+
+    // Find SubMenu
+    let findSubMenuItem = [menu addItemWithTitle:NSLocalizedString(@"Find", @"Main Menu Item Title") action:nil keyEquivalent:@""];
+    let findMenu = [[NSMenu alloc] initWithTitle:NSLocalizedString(@"Find", @"Main Menu Item Title")];
+
+    var findMenuItem = [findMenu addItemWithTitle:NSLocalizedString(@"Find", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"f"];
+    findMenuItem.tag = NSFindPanelActionShowFindPanel;
+
+    findMenuItem = [findMenu addItemWithTitle:NSLocalizedString(@"Find Next", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"g"];
+    findMenuItem.tag = NSFindPanelActionNext;
+
+    findMenuItem = [findMenu addItemWithTitle:NSLocalizedString(@"Find Previous", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"G"];
+    findMenuItem.tag = NSFindPanelActionPrevious;
+
+    findMenuItem = [findMenu addItemWithTitle:NSLocalizedString(@"Use Selection for Find", @"Main Menu Item Title") action:@selector(performFindPanelAction:) keyEquivalent:@"e"];
+    findMenuItem.tag = NSFindPanelActionSetFindString;
+
+    [findMenu addItemWithTitle:NSLocalizedString(@"Jump to Selection", @"Main Menu Item Title") action:@selector(centerSelectionInVisibleArea:) keyEquivalent:@"j"];
+    [menu setSubmenu:findMenu forItem:findSubMenuItem];
+
+    let menuItem = NSMenuItem.new;
+    menuItem.submenu = menu;
+
+    return menuItem;
+}
+
+- (NSMenuItem *)_viewSubmenu {
+    // Build submenu
+    let localizedMenuString = [NSBundle localizedMenuStringForKey:@"View"];
+    let menu = [[NSMenu alloc] initWithTitle:localizedMenuString];
+
+    let fullScreenMenuItem = [menu addItemWithTitle:NSLocalizedString(@"Enter Full Screen", @"Main Menu Item Title") action:@selector(toggleFullScreen:) keyEquivalent:@"f"];
+    fullScreenMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand | NSEventModifierFlagControl;
+
+    let menuItem = [[NSMenuItem alloc] initWithTitle:localizedMenuString action:NULL keyEquivalent:@""];
+    menuItem.submenu = menu;
+
+    return menuItem;
+}
+
+- (NSMenuItem *)_windowSubmenu {
+    let appDelegate = (CCNApplicationDelegate *)NSApp.delegate;
+
+    // Build submenu
+    let localizedMenuString = NSLocalizedString(@"Window", @"Main Menu Item Title");
+    let menu = [[NSMenu alloc] initWithTitle:localizedMenuString];
+
+    [menu addItemWithTitle:NSLocalizedString(@"Minimize", @"Main Menu Item Title") action:@selector(performMiniaturize:) keyEquivalent:@"m"];
+    [menu addItemWithTitle:NSLocalizedString(@"Zoom", @"Main Menu Item Title") action:@selector(performZoom:) keyEquivalent:@""];
+    [menu addItem:NSMenuItem.separatorItem];
+
+    var overviewMenuItem = [menu addItemWithTitle:NSLocalizedString(@"Podcast Overview", @"Main Menu Item Title") action:@selector(populateMainWindow) keyEquivalent:@"0"];
+    overviewMenuItem.keyEquivalentModifierMask = NSEventModifierFlagCommand | NSEventModifierFlagShift;
+    overviewMenuItem.target = appDelegate;
+
+    let menuItem = [[NSMenuItem alloc] initWithTitle:localizedMenuString action:NULL keyEquivalent:@""];
+    menuItem.submenu = menu;
+    NSApp.windowsMenu = menu;
+
+    return menuItem;
+}
+
+- (NSMenuItem *)_helpSubmenu {
+    // Build submenu
+    let localizedMenuString = [NSBundle localizedMenuStringForKey:@"Help"];
+    let menu = [[NSMenu alloc] initWithTitle:localizedMenuString];
+    let applicationHelpMenuItem = [menu addItemWithTitle:[NSString stringWithFormat:@"%@ %@", NSApplication.applicationName, localizedMenuString]
+                                                  action:@selector(showHelp:)
+                                           keyEquivalent:@"?"];
+    applicationHelpMenuItem.target = NSApp;
+    
+    let menuItem = [[NSMenuItem alloc] initWithTitle:localizedMenuString action:NULL keyEquivalent:@""];
+    menuItem.submenu = menu;
+    NSApp.helpMenu = menu;
+
+    return menuItem;
+}
+
+// MARK: - Private Helper
+
+- (NSMenuItem *)_menuItemWithTitle:(NSString *)title action:(SEL)action target:(id)target {
+    let menuItem = NSMenuItem.new;
+    menuItem.title = title;
+    menuItem.action = action;
+    menuItem.target = target;
+
+    return menuItem;
 }
 
 @end
